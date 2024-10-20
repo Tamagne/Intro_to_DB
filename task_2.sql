37e65739c886a967ac0ca331937f12ed2a70701a
Tamagne <127195999+Tamagne@users.noreply.github.com>
2024-10-21 00:37:42 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -8,10 +8,7 @@
    order details
    Name of the file should be task_2.sql
 */
-
-CREATE DATABASE IF NOT EXISTS ALX_BOOK_STORE;
-USE ALX_BOOK_STORE;
-
+USE alx_book_store;
 CREATE TABLE CUSTOMERS (
     CUSTOMER_ID INT PRIMARY KEY,       -- Unique identifier for each customer
     CUSTOMER_NAME VARCHAR(215),        -- Customer's name
@@ -27,14 +24,4 @@ CREATE TABLE AUTHORS (
 CREATE TABLE BOOKS (
     BOOK_ID INT PRIMARY KEY,           -- Unique identifier for each book
     TITLE VARCHAR(130),                -- Title of the book
-    AUTHOR_ID INT,                     -- Foreign key referencing AUTHORS
-    PRICE DOUBLE,                      -- Price of the book
-    PUBLICATION_DATE DATE,             -- Publication date of the book
-    FOREIGN KEY (AUTHOR_ID) REFERENCES AUTHORS (AUTHOR_ID)  -- Establish foreign key relationship
-); 
-
-CREATE TABLE ORDERS (
-    ORDER_ID INT PRIMARY KEY,          -- Unique identifier for each order
-    CUSTOMER_ID INT,                   -- Foreign key referencing CUSTOMERS
-    ORDER_DATE DATE,     
-
+    AUTHOR_ID INT,             
